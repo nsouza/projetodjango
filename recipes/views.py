0442e8f77f0bd99951f1f27f5f3a4de4cdc7a79c
Nilo Souza <nilo.contatos@gmail.com>
2025-08-31 05:41:17 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,5 @@
-#from django.shortcuts import render
-import re
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', content={'name': 'Nilo Souza'}) 
 
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
